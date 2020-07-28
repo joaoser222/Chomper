2b8c61faa6684061e7fcdd679e3c12097125610e
joaoser222 <joaoser222@gmail.com>
2020-07-28 18:57:32 -0300

    Adc. seed para admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(
+  name: 'admin',
+  email: 'admin@chomper.com',
+  password: '123456789',
+  kind: 'admin',
+  latitude: -15.7744227,
+  longitude: -48.0772946
+)
