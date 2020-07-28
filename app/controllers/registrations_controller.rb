cc30a1fdf7090ae50eb53bff6c4933fd5cef3cb3
joaoser222 <joaoser222@gmail.com>
2020-07-28 18:45:39 -0300

    Adc. método para alteração de perfil

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,27 @@
 class RegistrationsController < Devise::RegistrationsController
   respond_to :json
+  before_action :authenticate_user!, only: [:profile,:upload_photo]
 
-  def create
-    build_resource(sign_up_params)
+  def profile
+    user = User.find(current_user.id)
+    user.update(item_params.except(:kind))
+    if user.valid?
+      render json: { success: 'Usuário alterado com sucesso!' }
+    else
+      render json: user.errors.messages, status: 422
+    end
+  end
 
-    resource.save
+  def upload_photo
+    uploaded_file = params[:avatar]
+    filename = "uploads/#{uploaded_file.original_filename}"
+    File.open("public/#{filename}", 'wb') do |file|
+      file.write(uploaded_file.read)
+    end
+    if User.where(id: current_user.id).update(avatar: filename)
+      head :ok
+    end
+  end
 
     if resource.errors.empty?
       render json: resource
