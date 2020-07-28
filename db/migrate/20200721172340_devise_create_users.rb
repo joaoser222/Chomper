d9d480e94a76734df80c6f7b61999a23c21b4676
joaoser222 <joaoser222@gmail.com>
2020-07-28 18:58:04 -0300

    Adc. campos para dados pessoais e reset de senhas

--- db/migrate/20200721172340_devise_create_users.rb
+++ db/migrate/20200721172340_devise_create_users.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class DeviseCreateUsers < ActiveRecord::Migration[6.0]
+
   def change
     create_table :users do |t|
       ## Database authenticatable
@@ -8,24 +9,30 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :encrypted_password, null: false, default: ''
       t.string :jti,                null: false, default: ''
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      t.string                      :reset_password_token
+      t.datetime                    :reset_password_sent_at
 
       ## Rememberable
-      t.datetime :remember_created_at
+      t.datetime                    :remember_created_at
 
-      ## Personal informations
+      ## Personal data
       t.string :name,               null: false, default: ''
       t.string :avatar,             null: true
-      t.string :localization,       null: false
-      t.string :type,               null: false, default: ''
-
+      t.string :phone,              null: true
+      t.string :document,           null: true
+      t.string :state,              null: true
+      t.string :city,               null: true
+      t.string :district,           null: true
+      t.string :street,             null: true
+      t.decimal :latitude,          null: false, precision: 10, scale: 8
+      t.decimal :longitude,         null: false, precision: 11, scale: 8
+      t.decimal :radius,            null: false, default: 0
+      t.string :kind,               null: false, default: ''
       t.timestamps                  null: false
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    add_index :users, :jti, unique: true
   end
 end
