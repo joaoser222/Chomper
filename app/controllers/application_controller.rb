260423a5ec743f390bdcc734204ee671d644640c
joaoser222 <joaoser222@gmail.com>
2020-07-28 18:44:21 -0300

    Adc. func. set_permission para barrar usuários

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  def set_permission(permission)
+    if (!(permission.include? current_user.kind))
+      render json: {error: 'Usuário logado não possui permissão para esta ação'}, status: 401
+    end
+  end
 end
