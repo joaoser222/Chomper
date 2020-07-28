4198bbc041c4a2250520e7f5a30c33d18e855a45
joaoser222 <joaoser222@gmail.com>
2020-07-28 18:57:16 -0300

    Mod. pela migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_21_185321) do
+ActiveRecord::Schema.define(version: 2020_07_28_193845) do
 
   create_table "adverts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "description", default: "", null: false
     t.string "status", default: "a", null: false
     t.decimal "price", precision: 10, scale: 3, null: false
@@ -21,6 +22,12 @@ ActiveRecord::Schema.define(version: 2020_07_21_185321) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_adverts_on_user_id"
+  end
+
+  create_table "jwt_denylist", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -32,11 +39,20 @@ ActiveRecord::Schema.define(version: 2020_07_21_185321) do
     t.datetime "remember_created_at"
     t.string "name", default: "", null: false
     t.string "avatar"
-    t.string "localization", null: false
-    t.string "type", default: "", null: false
+    t.string "phone"
+    t.string "document"
+    t.string "state"
+    t.string "city"
+    t.string "district"
+    t.string "street"
+    t.decimal "latitude", precision: 10, scale: 8, null: false
+    t.decimal "longitude", precision: 11, scale: 8, null: false
+    t.decimal "radius", precision: 10, default: "0", null: false
+    t.string "kind", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
