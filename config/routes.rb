a86faca597177e3799064414adc0db23ba101bb1
joaoser222 <joaoser222@gmail.com>
2020-07-28 18:53:23 -0300

    Mod. rotas de devise/users

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   resources :adverts
-  devise_for :users,
-              path: '',
-              path_names: {
-                sign_in: 'login',
-                sign_out: 'logout',
-                registration: 'signup'
-              },
-              controllers: {
-                sessions: 'sessions',
-                registrations: 'registrations'
-              }
+  devise_for :users,skip: [:sessions, :registrations, :passwords]
+  devise_scope :user do
+    post 'login', to: 'sessions#create', as: :user_session
+    delete 'logout', to: 'sessions#logout', as: :destroy_user_session
+    post 'password/reset', to: 'registrations#password_reset'
+    post 'password/edit', to: 'registrations#password_edit'
+    post 'register/customer', to: 'registrations#register'
+    post 'register/company', to: 'registrations#register'
+    put 'profile', to: 'registrations#profile'
+    post 'profile/upload_photo', to: 'registrations#upload_photo'
+  end
 end
