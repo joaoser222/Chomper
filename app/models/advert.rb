19687fdc3bed0b85fd6becddafd2f64da279fffb
joaoser222 <joaoser222@gmail.com>
2020-07-28 18:48:18 -0300

    Adc. model para anúncios

--- app/models/advert.rb
+++ app/models/advert.rb
@@ -1,2 +1,23 @@
 class Advert < ApplicationRecord
+  include Filterable
+  belongs_to :user
+  validates :name,
+            length: { in: 5..50 },
+            presence: true,
+            allow_nil: :updated_at_changed?
+
+  validates :description,
+            length: { in: 10..100 },
+            presence: true,
+            allow_nil: :updated_at_changed?
+
+  validates :price,
+            presence: true,
+            numericality: true,
+            allow_nil: :updated_at_changed?
+
+  validates :status,
+            length: { is: 1 },
+            presence: true,
+            allow_nil: :updated_at_changed?
 end
