73afbb80d7571efa3b4254acd755a7344cb1c644
joaoser222 <joaoser222@gmail.com>
2020-07-28 18:43:02 -0300

    Adicionado métodos para facilitar modularidade

--- app/controllers/actions_helper.rb
+++ app/controllers/actions_helper.rb
@@ -1,29 +1,22 @@
 # Este Helper e responsavel por implementar as acoes de Crud nos controllers.
-# Ao inclui-lo no controller e nescessario declarar a variavel @model
-# na inicializacao da classe e as funcoes privadas items_params e set_item
+# Ao inclui-lo no controller e nescessario declarar a variavel @model e @fields
+# na inicializacao da classe e as funcoes items_params e set_item
 #
 module ActionsHelper
-
+  # INDEX
   def index
-    data = @model.all.paginate(page: params[:page], per_page: params[:itens])
-    render json: {
-      current_page: data.current_page,
-      total_pages: data.total_pages,
-      per_page: data.per_page,
-      total_items: data.total_entries,
-      items: data
-    }
+    render json: self.pagination(@model.filter(filters.merge(modifiers)))
   end
 
   # GET
   def show
-    render json: @model.find_by(id: params[:id])
+    render json: @item
   end
 
   # POST
   def create
-    @item = @model.new(item_params)
-    if @item.save
+    @item = @model.create(item_params.merge(modifiers))
+    if @item.valid?
       render json: @item.to_json
     else
       render json: @item.errors, status: :unprocessable_entity
@@ -32,7 +25,8 @@ module ActionsHelper
 
   # PATCH/PUT
   def update
-    if @item.update(item_params)
+    @item.update(item_params)
+    if @item.valid?
       render json: @item
     else
       render json: @item.errors, status: :unprocessable_entity
@@ -42,5 +36,39 @@ module ActionsHelper
   # DELETE
   def destroy
     @item.destroy
+  end
+
+  # Associa o item que será visualizado, modificado ou excluido
+  def set_item
+    @item = @model.find_by({id: params[:id]}.merge(modifiers))
+  end
+
+  # Define os campos permitidos no objeto que será salvo
+  def item_params
+    params.require(:"#{@model}".downcase).permit(@fields)
+  end
+
+  # Define os campos de filtros permitidos
+  def filters
+    params.permit(@fields)
+  end
+
+  # Facilita a paginacao dos itens
+  def pagination(data)
+    pages = data.paginate(page: params[:page], per_page: params[:itens])
+    {
+      current_page: pages.current_page,
+      total_pages: pages.total_pages,
+      per_page: pages.per_page,
+      total_items: pages.total_entries,
+      items: pages
+    }
+  end
+
+  # Sobrescreve itens com valor padrao, impedindo payloads com valores
+  # Nao permitidos
+
+  def modifiers
+    {}
   end
 end
